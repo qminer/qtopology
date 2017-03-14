5cae5ae1363b1e63dc1d047b27bb24584bb4fa01
Viktor Jovanoski <viktor@carvic.si>
2017-03-14 10:00:05 +0100

    Added tests for console bolt

---
+++ tests/std_nodes/console_bolt.tests.js
@@ -0,0 +1,50 @@
+"use strict";
+
+/*global describe, it, before, beforeEach, after, afterEach */
+
+const assert = require("assert");
+const cb = require("../../src/std_nodes/console_bolt");
+
+describe('ConsoleBolt', function () {
+    it('constructable', function () {
+        let target = new cb.ConsoleBolt();
+    });
+    it('init passes', function (done) {
+        let emited = [];
+        let name = "some_name";
+        let config = {
+            onEmit: (data, stream_id, callback) => {
+                emited.push({ data, stream_id });
+                callback();
+            }
+        };
+        let target = new cb.ConsoleBolt();
+        target.init(name, config, (err) => {
+            assert.ok(!err);
+            done();
+        });
+    });
+    it('receive - must pass through', function (done) {
+        let emited = [];
+        let name = "some_name";
+        let xdata = { test: true };
+        let xstream_id = null;
+        let config = {
+            onEmit: (data, stream_id, callback) => {
+                emited.push({ data, stream_id });
+                callback();
+            }
+        };
+        let target = new cb.ConsoleBolt();
+        target.init(name, config, (err) => {
+            assert.ok(!err);
+            target.receive(xdata, xstream_id, (err) => {
+                assert.ok(!err);
+                assert.equal(emited.length, 1);
+                assert.deepEqual(emited[0].data, xdata);
+                assert.equal(emited[0].stream_id, xstream_id);
+                done();
+            });
+        });
+    });
+});
