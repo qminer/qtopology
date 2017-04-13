c81cd8b67726cdebe93c206b13d239108aa6de43
Viktor Jovanoski <viktor@carvic.si>
2017-04-13 13:22:44 +0200

    Another fix of injection

--- src/topology_compiler.js
+++ src/topology_compiler.js
@@ -34,17 +34,19 @@ class TopologyCompiler {
     compile() {
         let vars = this._config.variables || {};
         if (this._config.general.initialization) {
-            let init_top = this._config.general.initialization;
-            init_top.working_dir = injectVars(init_top.working_dir, vars);
-            if (init_top.init) {
-                init_top.init = injectVars(init_top.init, vars);
+            for (let init_top of this._config.general.initialization) {
+                init_top.working_dir = injectVars(init_top.working_dir, vars);
+                if (init_top.init) {
+                    init_top.init = injectVars(init_top.init, vars);
+                }
             }
         }
         if (this._config.general.shutdown) {
-            let shutdown_top = this._config.general.shutdown;
-            shutdown_top.working_dir = injectVars(shutdown_top.working_dir, vars);
-            if (shutdown_top.init) {
-                shutdown_top.init = injectVars(shutdown_top.init, vars);
+            for (let shutdown_top of this._config.general.shutdown) {
+                shutdown_top.working_dir = injectVars(shutdown_top.working_dir, vars);
+                if (shutdown_top.init) {
+                    shutdown_top.init = injectVars(shutdown_top.init, vars);
+                }
             }
         }
 
