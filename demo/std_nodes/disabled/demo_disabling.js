f30f2838f9ee974dbeb4fa79ffffafc2bb260a17
Viktor Jovanoski <viktor@carvic.si>
2017-05-31 14:01:55 +0200

    Added flag disabled to edges of the topology

---
+++ demo/std_nodes/disabled/demo_disabling.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const async = require("async");
+const qtopology = require("../../..");
+
+let config = require("./topology.json");
+qtopology.validate({ config: config, exitOnError: true });
+let topology = new qtopology.TopologyLocal();
+
+async.series(
+    [
+        (xcallback) => {
+            topology.init(config, xcallback);
+        },
+        (xcallback) => {
+            console.log("Init done");
+            topology.run();
+            setTimeout(function () {
+                xcallback();
+            }, 5000);
+        },
+        (xcallback) => {
+            console.log("Starting shutdown sequence...");
+            topology.shutdown(xcallback);
+        }
+    ],
+    (err) => {
+        if (err) {
+            console.log("Error in shutdown", err);
+        }
+        console.log("Finished.");
+    }
+);
+
+
+function shutdown(err) {
+
+    if (err) {
+        console.log("Error", err);
+    }
+    if (topology) {
+        topology.shutdown((err) => {
+            if (err) {
+                console.log("Error", err);
+            }
+            process.exit(1);
+        });
+        topology = null;
+    }
+}
+
+//do something when app is closing
+process.on('exit', shutdown);
+
+//catches ctrl+c event
+process.on('SIGINT', shutdown);
+
+//catches uncaught exceptions
+process.on('uncaughtException', (err) => { shutdown(err); });
