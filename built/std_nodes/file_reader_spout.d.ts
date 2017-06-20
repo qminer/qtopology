311508e0681f2fcb183387f64311fc24b9532886
Viktor Jovanoski <viktor@carvic.si>
2017-06-20 15:19:37 +0200

    Commented out unused code, it might be useful in the future.

--- built/std_nodes/file_reader_spout.d.ts
+++ built/std_nodes/file_reader_spout.d.ts
@@ -1,27 +1,5 @@
 import * as intf from "../topology_interfaces";
 /** This spout reads input file in several supported formats and emits tuples. */
-export declare class FileReaderSpout2 implements intf.Spout {
-    private name;
-    private stream_id;
-    private file_format;
-    private file_name;
-    private csv_separator;
-    private csv_fields;
-    private csv_header;
-    private tuples;
-    private should_run;
-    constructor();
-    init(name: string, config: any, context: any, callback: intf.SimpleCallback): void;
-    heartbeat(): void;
-    shutdown(callback: intf.SimpleCallback): void;
-    run(): void;
-    pause(): void;
-    next(callback: intf.SpoutNextCallback): void;
-    private processLineJson(content);
-    private processLineRaw(content);
-    private processLineCsv(line);
-}
-/** This spout reads input file in several supported formats and emits tuples. */
 export declare class FileReaderSpout implements intf.Spout {
     private name;
     private stream_id;
--- built/std_nodes/file_reader_spout.js
+++ built/std_nodes/file_reader_spout.js
@@ -1,106 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const sh = require("../util/stream_helpers");
 const fs = require("fs");
 const rl = require("readline");
 const high_water = 5000;
 const low_water = 50;
-/** This spout reads input file in several supported formats and emits tuples. */
-class FileReaderSpout2 {
-    constructor() {
-        this.name = null;
-        this.stream_id = null;
-        this.file_format = null;
-        this.tuples = null;
-        this.file_name = null;
-        this.should_run = false;
-    }
-    init(name, config, context, callback) {
-        this.name = name;
-        this.stream_id = config.stream_id;
-        this.file_name = config.file_name;
-        this.file_format = config.file_format || "json";
-        this.tuples = [];
-        if (this.file_format == "csv") {
-            this.csv_separator = config.separator || ",";
-            this.csv_fields = config.fields;
-        }
-        sh.importFileByLine(this.file_name, {
-            addLine: (line) => {
-                if (this.file_format == "json") {
-                    this.processLineJson(line);
-                }
-                else if (this.file_format == "csv") {
-                    this.processLineCsv(line);
-                }
-                else if (this.file_format == "raw") {
-                    this.processLineRaw(line);
-                }
-            }
-        }, null);
-        callback();
-    }
-    heartbeat() { }
-    shutdown(callback) {
-        callback();
-    }
-    run() {
-        this.should_run = true;
-    }
-    pause() {
-        this.should_run = false;
-    }
-    next(callback) {
-        if (!this.should_run) {
-            return callback(null, null, null);
-        }
-        if (this.tuples.length === 0) {
-            return callback(null, null, null);
-        }
-        let data = this.tuples[0];
-        this.tuples = this.tuples.slice(1);
-        callback(null, data, this.stream_id);
-    }
-    processLineJson(content) {
-        let lines = content.split("\n");
-        for (let line of lines) {
-            line = line.trim();
-            if (line.length == 0)
-                continue;
-            this.tuples.push(JSON.parse(line));
-        }
-    }
-    processLineRaw(content) {
-        let lines = content.split("\n");
-        for (let line of lines) {
-            line = line.trim().replace("\r", "");
-            if (line.length == 0)
-                return;
-            this.tuples.push({ content: line });
-        }
-    }
-    processLineCsv(line) {
-        if (!this.csv_header) {
-            this.csv_header = line.split(this.csv_separator);
-            return;
-        }
-        else {
-            line = line.trim();
-            if (line.length == 0)
-                return;
-            let values = line.split(this.csv_separator);
-            let result = {};
-            for (let i = 0; i < this.csv_header.length; i++) {
-                let name = this.csv_header[i];
-                if (!this.csv_fields || this.csv_fields.indexOf(name) >= 0) {
-                    result[name] = values[i];
-                }
-            }
-            this.tuples.push(result);
-        }
-    }
-}
-exports.FileReaderSpout2 = FileReaderSpout2;
 /** This spout reads input file in several supported formats and emits tuples. */
 class FileReaderSpout {
     constructor() {
@@ -207,4 +110,106 @@ class FileReaderSpout {
     }
 }
 exports.FileReaderSpout = FileReaderSpout;
+// /** This spout reads input file in several supported formats and emits tuples. */
+// export class FileReaderSpout2 implements intf.Spout {
+//     private name: string;
+//     private stream_id: string;
+//     private file_format: string;
+//     private file_name: string;
+//     private csv_separator: string;
+//     private csv_fields: string[];
+//     private csv_header: string[];
+//     private tuples: any[];
+//     private should_run: boolean;
+//     constructor() {
+//         this.name = null;
+//         this.stream_id = null;
+//         this.file_format = null;
+//         this.tuples = null;
+//         this.file_name = null;
+//         this.should_run = false;
+//     }
+//     init(name: string, config: any, context: any, callback: intf.SimpleCallback) {
+//         this.name = name;
+//         this.stream_id = config.stream_id;
+//         this.file_name = config.file_name;
+//         this.file_format = config.file_format || "json";
+//         this.tuples = [];
+//         if (this.file_format == "csv") {
+//             this.csv_separator = config.separator || ",";
+//             this.csv_fields = config.fields;
+//         }
+//         sh.importFileByLine(
+//             this.file_name,
+//             {
+//                 addLine: (line: string) => {
+//                     if (this.file_format == "json") {
+//                         this.processLineJson(line);
+//                     } else if (this.file_format == "csv") {
+//                         this.processLineCsv(line);
+//                     } else if (this.file_format == "raw") {
+//                         this.processLineRaw(line);
+//                     }
+//                 }
+//             },
+//             null
+//         );
+//         callback();
+//     }
+//     heartbeat() { }
+//     shutdown(callback: intf.SimpleCallback) {
+//         callback();
+//     }
+//     run() {
+//         this.should_run = true;
+//     }
+//     pause() {
+//         this.should_run = false;
+//     }
+//     next(callback: intf.SpoutNextCallback) {
+//         if (!this.should_run) {
+//             return callback(null, null, null);
+//         }
+//         if (this.tuples.length === 0) {
+//             return callback(null, null, null);
+//         }
+//         let data = this.tuples[0];
+//         this.tuples = this.tuples.slice(1);
+//         callback(null, data, this.stream_id);
+//     }
+//     private processLineJson(content: string) {
+//         let lines = content.split("\n");
+//         for (let line of lines) {
+//             line = line.trim();
+//             if (line.length == 0) continue;
+//             this.tuples.push(JSON.parse(line));
+//         }
+//     }
+//     private processLineRaw(content: string) {
+//         let lines = content.split("\n");
+//         for (let line of lines) {
+//             line = line.trim().replace("\r", "");
+//             if (line.length == 0) return;
+//             this.tuples.push({ content: line });
+//         }
+//     }
+//     private processLineCsv(line: string) {
+//         if (!this.csv_header) {
+//             this.csv_header = line.split(this.csv_separator);
+//             return;
+//         } else {
+//             line = line.trim();
+//             if (line.length == 0) return;
+//             let values = line.split(this.csv_separator);
+//             let result = {};
+//             for (let i = 0; i < this.csv_header.length; i++) {
+//                 let name = this.csv_header[i];
+//                 if (!this.csv_fields || this.csv_fields.indexOf(name) >= 0) {
+//                     result[name] = values[i];
+//                 }
+//             }
+//             this.tuples.push(result);
+//         }
+//     }
+// }
 //# sourceMappingURL=file_reader_spout.js.map
