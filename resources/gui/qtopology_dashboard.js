e56bf45799b337d87c69fe6e04db78c354c85212
Viktor Jovanoski <viktor@carvic.si>
2017-06-26 11:20:06 +0200

    More gui work

--- resources/gui/qtopology_dashboard.js
+++ resources/gui/qtopology_dashboard.js
@@ -4,6 +4,12 @@ function QTopologyDashboardViewModel(divIdTarget) {
     this.workers = ko.observableArray();
     this.topologies = ko.observableArray();
     this.storage_props = ko.observableArray();
+
+    this.bladeWorker = "bladeWorker";
+    this.bladeTopology = "bladeTopology";
+
+    this.blades = [this.bladeWorker, this.bladeTopology];
+    this.prepareBlades();
 }
 
 QTopologyDashboardViewModel.prototype.post = function (cmd, data, callback) {
@@ -24,12 +30,14 @@ QTopologyDashboardViewModel.prototype.init = function (callback) {
     self.post("worker-status", {}, function (data) {
         self.workers.removeAll();
         for (let d of data) {
+            d.open = function () { self.showWorkerInfo(d.name); };
             self.workers.push(d);
         }
     });
     self.post("topology-status", {}, function (data) {
         self.topologies.removeAll();
         for (let d of data) {
+            d.open = function () { self.showTopologyInfo(d.uuid); };
             self.topologies.push(d);
         }
     });
@@ -39,4 +47,32 @@ QTopologyDashboardViewModel.prototype.init = function (callback) {
     if (callback) {
         callback();
     }
+}
+
+QTopologyDashboardViewModel.prototype.showBlade = function (name) {
+    //$("#" + name).toggle("slide", { direction: "right"}, 500);
+    $("#" + name).show({ duration: 500, easing: "swing" });
+}
+QTopologyDashboardViewModel.prototype.showWorkerInfo = function (name) {
+    //$("#" + this.bladeWorker).toggle("slide", { direction: "right"}, 500);
+    this.showBlade(this.bladeWorker);
+}
+QTopologyDashboardViewModel.prototype.showTopologyInfo = function (uuid) {
+    //$("#" + this.bladeTopology).toggle("slide", { direction: "right"}, 500);
+    this.showBlade(this.bladeTopology);
+}
+
+QTopologyDashboardViewModel.prototype.prepareBlades = function () {
+    var self = this;
+    for (var ii = 0; ii < self.blades.length; ii++) (function (i) {
+        var blade_name = self.blades[i];
+        var blade_obj = $("#" + blade_name);
+        blade_obj.hide();
+        // prepend close buttons
+        blade_obj.find(".blade-close").click(function () {
+            blade_obj.hide();
+        })
+        // var $input = $('<input id="btnClose' + blade_name + '" type="button" value="Close.." />');
+        // blade_obj.prepend(blade_obj);
+    })(ii);
 }
