c416cb2973795b968f576e8332b15b2f4e0f4a41
Viktor Jovanoski <viktor@carvic.si>
2017-10-26 11:25:42 +0200

    Improving callback mechanism and error handling for spouts

--- src/topology_local_inprocess.ts
+++ src/topology_local_inprocess.ts
@@ -221,23 +221,26 @@ export class TopologySpoutWrapper extends TopologyNodeBase {
                     this.child.next((err, data, stream_id, xcallback) => {
                         self.telemetryAdd(Date.now() - ts_start);
                         if (err) {
+                            // child sent an error, xcallback is ignored
                             log.logger().exception(err);
                             return callback(err);
                         }
                         if (!data) {
+                            // child didn't send any data, so xcallback is ignored
                             self.nextTs = Date.now() + 1 * 1000; // sleep for 1 sec if spout is empty
                             callback();
                         } else {
                             try {
                                 self.emitCallback(data, stream_id, (err) => {
                                     // in case child object expects confirmation call for this tuple
-                                    if (xcallback) {
-                                        xcallback(err, callback);
+                                    if (xcallback && !err) {
+                                        xcallback(null, callback);
                                     } else {
-                                        callback();
+                                        callback(err);
                                     }
                                 });
                             } catch (e) {
+                                // there was an error, don't call the child's xcallback
                                 callback(e);
                             }
                         }
