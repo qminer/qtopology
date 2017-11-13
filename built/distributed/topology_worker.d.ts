39c623f37c7867d2b2fd1e05c80b1a0c7203feca
Jan Rupnik <jan.rupnik@ijs.si>
2017-11-13 16:37:47 +0100

    worker error handling

--- built/distributed/topology_worker.d.ts
+++ built/distributed/topology_worker.d.ts
@@ -11,21 +11,30 @@ export declare class TopologyWorker {
     private waiting_for_shutdown;
     /** Initializes this object */
     constructor(name: string, storage: intf.CoordinationStorage, overrides?: object);
+    /** Internal wrapper around process.exit */
+    private exit(code);
     /** Starts this worker */
     run(): void;
     /** This method verifies that all topologies are running and properly registered */
     private resolveTopologyMismatches(uuids, callback);
+    /** Internal method ensures that a topology exits. */
+    private ensureExit(rec, err?);
     /** Internal method that creates proxy for given topology item */
-    private createProxy(rec);
-    /** Starts single topology */
+    private createInitAndRunProxy(rec);
+    /** Starts single topology.
+     * Guards itself from duplicated calls.
+     */
     private start(uuid, config);
+    private hasTopology(uuid);
     /** This method injects override values into variables section of the configuration. */
     private injectOverrides(config);
     /** Remove specified topology from internal list */
     private removeTopology(uuid);
     /** Shuts down the worker and all its subprocesses. */
     shutdown(callback: intf.SimpleCallback): void;
-    /** Sends shutdown signals to all topologies */
+    /** Sends shutdown signals to all topologies. Will try to shutdown
+     * all topologies and if any of them failed, return the first error.
+     */
     private shutDownTopologies(callback);
     /** Sends shut down signal to single topology */
     private shutDownTopology(uuid, do_kill, callback);
