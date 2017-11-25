1799a26a90624cc0172e4e0e49b78c35f3fc9bb5
bergloman <bergloman@gmail.com>
2017-11-26 00:01:54 +0100

    File-append-ex bolt

--- built/std_nodes/file_append_bolt_ex.js
+++ built/std_nodes/file_append_bolt_ex.js
@@ -8,130 +8,48 @@ const async = require("async");
 /////////////////////////////////////////////////////////////////////////////
 const injection_placeholder = "##INJECT##";
 const injection_placeholder_field = "##INJECT2##";
-// class GroupRec {
-//     ts_max: number;
-//     data: string[];
-//     file_name_current: string;
-// }
-/** This bolt writes incoming messages to file. Data is split into
- * several files, based on the value of a specific field.
- * Also, data is grouped into files based on timestamp field.
- */
-class FileAppendBoltEx {
-    constructor() {
-        this.name = null;
-        this.log_prefix = null;
-        this.current_data = new Map();
-        this.split_value = new Set();
-        this.current_file_contains_data = false;
-        this.split_by_field = null;
-    }
-    init(name, config, context, callback) {
-        this.name = name;
-        this.log_prefix = `[FileAppendBolt ${this.name}] `;
-        this.file_name_template = config.file_name_template;
-        this.prepend_timestamp = config.prepend_timestamp;
-        this.split_over_time = config.split_over_time;
-        this.split_period = config.split_period || 60 * 60 * 1000;
-        if (config.split_by_field) {
-            this.split_by_field = config.split_by_field.split(".");
-        }
-        this.compress = config.compress;
-        // prepare filename template for injection
-        if (this.split_over_time) {
-            let ext = path.extname(this.file_name_template);
-            this.next_split_after = Math.floor(Date.now() / this.split_period) * this.split_period;
-            this.file_name_template =
-                this.file_name_template.slice(0, this.file_name_template.length - ext.length) +
-                    "_" + injection_placeholder +
-                    (this.split_by_field ? "_" + injection_placeholder_field : "") +
-                    ext;
-        }
-        else {
-            this.file_name_current = this.file_name_template;
-            if (config.delete_existing) {
-                if (fs.existsSync(this.file_name_current)) {
-                    fs.unlinkSync(this.file_name_current);
-                }
-            }
-        }
-        callback();
-    }
-    toISOFormatLocal(d) {
-        let tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
+/** Class for utility methods */
+class Utils {
+    static toISOFormatLocal(d) {
+        let tzoffset = (new Date()).getTimezoneOffset() * 60000; // timezone offset in milliseconds
         let s = (new Date(d - tzoffset)).toISOString().slice(0, -1);
         return s;
     }
-    fileNameTimestampValue() {
-        let d = Math.floor(Date.now() / this.split_period) * this.split_period;
+    static fileNameTimestampValue(ts, split_period) {
+        let d = Math.floor(ts / split_period) * split_period;
         let s = this.toISOFormatLocal(d);
         s = s.slice(0, s.indexOf("."));
         s = s.replace(/\:/ig, "_").replace(/\-/ig, "_");
         return s;
     }
-    writeToFile(callback) {
-        if (this.current_data.size == 0)
-            return callback();
-        let d = Date.now();
-        let do_file_split = (this.split_over_time && this.next_split_after < d);
-        let self = this;
-        async.series([
-            (xcallback) => {
-                if (!do_file_split)
-                    return xcallback();
-                // perform compression of existing file if it exists.
-                // only used when file split will occur
-                // otherwise we just zip at shutdown.
-                this.zipCurrentFile(xcallback);
-            },
-            (xcallback) => {
-                if (!do_file_split)
-                    return xcallback();
-                // calculate new file name
-                self.current_file_contains_data = false;
-                self.file_name_current = self.file_name_template.replace(injection_placeholder, self.fileNameTimestampValue());
-                log.logger().log(`${self.log_prefix} new file generated: ${self.file_name_current}`);
-                self.next_split_after = d + self.split_period;
-                xcallback();
-            },
-            (xcallback) => {
-                // write data to current file
-                self.current_data.forEach((value, key) => {
-                    let lines = value;
-                    this.split_value.add(key);
-                    let fname = self.file_name_current.replace(injection_placeholder_field, key);
-                    for (let line of lines) {
-                        fs.appendFileSync(fname, line);
-                    }
-                });
-                self.current_data.clear();
-                self.current_file_contains_data = true;
-                xcallback();
-            },
-        ], callback);
+    /** Utility function for getting data from object */
+    static getValueFromObject(obj, field_path) {
+        for (let i = 0; i < field_path.length - 1; i++) {
+            obj = obj[field_path[i]];
+        }
+        return obj[field_path[field_path.length - 1]];
     }
-    /** Zip current file if it exists  */
-    zipCurrentFile(callback) {
-        let self = this;
-        if (self.compress && self.current_file_contains_data) {
-            let fnames = [];
-            self.split_value.forEach((value, key) => {
-                let fname = self.file_name_current.replace(injection_placeholder_field, key);
-                fnames.push(fname);
-            });
-            async.eachLimit(fnames, 3, (item, xcallback) => {
-                if (fs.existsSync(item)) {
-                    log.logger().log(`${self.log_prefix} compressing current file: ${item}`);
-                    self.zipFile(item, xcallback);
-                }
-                else {
-                    xcallback();
-                }
-            }, callback);
-        }
-        else {
-            callback();
-        }
+}
+/** Single-bucket handling class */
+class BucketHandler {
+    /** Constructor that intializes this object */
+    constructor(log_prefix, file_name_template, field_value, ts_start, split_period) {
+        this.log_prefix = log_prefix;
+        this.file_name_template = file_name_template
+            .replace(injection_placeholder_field, field_value);
+        this.split_period = split_period;
+        this.data = [];
+        this.setTsFields(ts_start);
+    }
+    /** Given arbitrary timestamp, calculates and sets the start
+     * and the end timestamps in internal members. */
+    setTsFields(ts_start) {
+        this.ts_start = Math.floor(ts_start / this.split_period) * this.split_period;
+        this.ts_max = this.ts_start + this.split_period;
+        this.file_name_current = this.file_name_template
+            .replace(injection_placeholder, Utils.fileNameTimestampValue(this.ts_start, this.split_period));
+        this.curr_file_contains_data = false;
+        log.logger().log(`${this.log_prefix} new file generated: ${this.file_name_current}`);
     }
     /** Perform low-level zipping */
     zipFile(fname, callback) {
@@ -164,41 +82,120 @@ class FileAppendBoltEx {
             callback(e);
         }
     }
-    heartbeat() {
-        this.writeToFile(() => { });
-    }
-    shutdown(callback) {
+    /** Writes pending data to file */
+    writeFile(fname, callback) {
+        // write if any pending data
+        if (this.data.length == 0) {
+            return callback();
+        }
         let self = this;
-        this.writeToFile((err) => {
+        let data = self.data;
+        self.data = [];
+        for (let line of data) {
+            fs.appendFileSync(fname, line);
+        }
+        this.curr_file_contains_data = true;
+        callback();
+    }
+    /** Flushes all data and closes the object */
+    closeCurrentFile(callback) {
+        let fname = this.file_name_current;
+        let self = this;
+        self.writeFile(fname, (err) => {
             if (err)
                 return callback(err);
-            if (self.compress && self.current_file_contains_data) {
-                self.zipCurrentFile(callback);
+            if (self.curr_file_contains_data) {
+                self.zipFile(fname, callback);
             }
             else {
                 callback();
             }
         });
     }
+    /** Closes this object */
+    flush(callback) {
+        this.writeFile(this.file_name_current, callback);
+    }
+    /** Closes this object */
+    close(callback) {
+        this.closeCurrentFile(callback);
+    }
+    /** Handles new incoming data record */
+    receive(ts, data, callback) {
+        let self = this;
+        if (self.ts_max < ts) {
+            self.closeCurrentFile((err) => {
+                if (err)
+                    return callback(err);
+                self.setTsFields(ts);
+                self.data.push(data);
+                callback();
+            });
+        }
+        else {
+            self.data.push(data);
+            callback();
+        }
+    }
+}
+/** This bolt writes incoming messages to file. Data is split into
+ * several files/buckets, based on the value of a specific field.
+ * Also, data is grouped into files based on timestamp field.
+ * ASSUMPTION: The data is sequential with respect to timestamp (within each bucket)
+ */
+class FileAppendBoltEx {
+    constructor() {
+        this.name = null;
+        this.log_prefix = null;
+        this.buckets = new Map();
+        this.split_by_field = null;
+        this.timestamp_field = null;
+    }
+    init(name, config, context, callback) {
+        this.name = name;
+        this.log_prefix = `[FileAppendBolt ${this.name}] `;
+        this.file_name_template = config.file_name_template;
+        this.split_period = config.split_period || 60 * 60 * 1000;
+        this.split_by_field = config.split_by_field.split(".");
+        this.timestamp_field = config.timestamp_field.split(".");
+        // prepare filename template for injection
+        let ext = path.extname(this.file_name_template);
+        this.file_name_template =
+            this.file_name_template.slice(0, this.file_name_template.length - ext.length) +
+                "_" + injection_placeholder +
+                "_" + injection_placeholder_field +
+                ext;
+        callback();
+    }
+    heartbeat() {
+        for (let bh of this.buckets.values()) {
+            bh.flush((err) => {
+                if (err) {
+                    log.logger().error("Error in flushing file-append");
+                    log.logger().exception(err);
+                }
+            });
+        }
+    }
+    shutdown(callback) {
+        let self = this;
+        async.each(Array.from(self.buckets.values()), (bh, xcallback) => {
+            bh.close(xcallback);
+        }, callback);
+    }
     receive(data, stream_id, callback) {
         let s = "";
-        if (this.prepend_timestamp) {
-            s += this.toISOFormatLocal(Date.now()) + " ";
-        }
         s += JSON.stringify(data);
-        let key = "";
-        if (this.split_by_field) {
-            let obj = data;
-            for (let i = 0; i < this.split_by_field.length - 1; i++) {
-                obj = obj[this.split_by_field[i]];
-            }
-            key = obj[this.split_by_field[this.split_by_field.length - 1]];
+        let obj = data;
+        let key = Utils.getValueFromObject(obj, this.split_by_field);
+        let ts = Utils.getValueFromObject(obj, this.timestamp_field);
+        ts = (new Date(ts)).getTime();
+        // create new bucket if needed
+        if (!this.buckets.has(key)) {
+            let bh = new BucketHandler(this.log_prefix, this.file_name_template, key, ts, this.split_period);
+            this.buckets.set(key, bh);
         }
-        if (!this.current_data.has(key)) {
-            this.current_data.set(key, []);
-        }
-        this.current_data.get(key).push(s + "\n");
-        callback();
+        this.buckets.get(key).receive(ts, s + "\n", callback);
     }
 }
 exports.FileAppendBoltEx = FileAppendBoltEx;
