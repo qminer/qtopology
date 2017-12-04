5e01ea44f525ae172bd72e8291e06169e5b4e99a
bergloman <bergloman@gmail.com>
2017-12-04 23:55:14 +0100

    Removed commented code

--- src/std_nodes/process_spout.ts
+++ src/std_nodes/process_spout.ts
@@ -127,46 +127,4 @@ export class ProcessSpout implements intf.Spout {
         this.tuples = this.tuples.slice(1);
         callback(null, data, this.stream_id);
     }
-
-    // private readJsonFile(content: string) {
-    //     let lines = content.split("\n");
-    //     for (let line of lines) {
-    //         line = line.trim();
-    //         if (line.length == 0) continue;
-    //         this.tuples.push(JSON.parse(line));
-    //     }
-    // }
-
-    // private readRawFile(content: string) {
-    //     let lines = content.split("\n");
-    //     for (let line of lines) {
-    //         line = line.trim().replace("\r", "");
-    //         if (line.length == 0) continue;
-    //         this.tuples.push({ content: line });
-    //     }
-    // }
-
-    // private readCsvFile(content: string) {
-    //     let lines = content.split("\n");
-
-    //     // if CSV file contains header, use it.
-    //     // otherwise, the first line already contains data
-    //     if (this.csv_has_header) {
-    //         // read first list and parse fields names
-    //         let header = lines[0].replace("\r", "");
-    //         this.csv_fields = header.split(this.csv_separator);;
-    //         lines = lines.slice(1);
-    //     }
-
-    //     for (let line of lines) {
-    //         line = line.trim().replace("\r", "");
-    //         if (line.length == 0) continue;
-    //         let values = line.split(this.csv_separator);
-    //         let result = {};
-    //         for (let i = 0; i < this.csv_fields.length; i++) {
-    //             result[this.csv_fields[i]] = values[i];
-    //         }
-    //         this.tuples.push(result);
-    //     }
-    // }
 }
