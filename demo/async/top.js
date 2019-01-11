591849486dc8cf2f6613c2ee8527aa6517b44574
Viktor Jovanoski <bergloman@gmail.com>
2019-01-11 08:26:14 +0100

    Improving shutdown

--- demo/async/top.js
+++ demo/async/top.js
@@ -1,9 +1,7 @@
 "use strict";
 
 const qtopology = require("../..");
-const t = qtopology.runLocalTopologyFromFile("./topology.json");
+const shutdown = qtopology.runLocalTopologyFromFile("./topology.json");
 
 // let topology run for 5 seconds
-setTimeout(() => {
-    process.kill(process.pid);
-}, 5 * 1000);
+setTimeout(shutdown, 5 * 1000);
